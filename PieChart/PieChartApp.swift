cb0b192e3062f7b1e5e8118d7e65dc63e2df3527
SardorbekSaydamatov <sardorbek.saydamatov@mail.ru>
2024-08-31 11:27:37 +0500

    Initial Commit

---
+++ PieChart/PieChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieChartApp.swift
+//  PieChart
+//
+//  Created by Sardorbek Saydamatov on 31/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PieChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
